30cd61906b5b035bc328bb808d371b7414fdc416
Student <student@Makerss-MacBook-Pro.local>
2019-12-03 09:42:13 +0000

    2 passing tests

--- spec/middle_letters_spec.rb
+++ spec/middle_letters_spec.rb
@@ -4,10 +4,15 @@ describe "get_middle" do
   it "should return a full single letter word" do
     expect(get_middle("A")).to eq "A"
   end
+
+  it "should return a full two letter word" do
+    expect(get_middle("of")).to eq "of"
+  end
 end
 
+
 # 
-# get_middle("of") # => "of"
+# 
 # get_middle("test") # => "es"
 # get_middle("middle") # => "dd"
 # get_middle("testing") # => "t"
